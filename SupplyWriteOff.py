4b2a9635c2f2383ac7980602a306aa7c43712793
amandameirecosta <amandahn.meire@hotmail.com>
2022-07-19 17:27:47 -0300

    writeOff

--- SupplyWriteOff.py
+++ SupplyWriteOff.py
@@ -6,7 +6,7 @@ def eat(pandasSeries, v):
     for i in range(len(pandasSeries)):
         if pandasSeries[i] - v >= 0:
             pandasSeries[i] -=  v
-            v = max(v - pandasSeries[i], 0)
+            v = 0
 
         else:
             v -= pandasSeries[i]
@@ -23,7 +23,7 @@ def calculateWriteOffs(dictMateriais, df):
 
         for key in list(df.keys()):
             
-            print(key)
+            #print(key)
             if key == material:
                 try:
                     meses =list(df[key]['Meses'])
@@ -39,20 +39,25 @@ def calculateWriteOffs(dictMateriais, df):
                         batchExpirationDict[batch] = limitSalesDate
 
                     orderedBatchList = sorted(batchExpirationDict.items(), key=lambda item: item[1])  
-                    for batch in orderedBatchList:    
-                        limitMonth = meses[0]
+                    for batch in orderedBatchList:
+                        print(limitSalesDate)    
+                        limitMonth = None
                         for m in meses:
                             dateObj = datetime.strptime(m.lower(), "%b %Y")
                             if dateObj < limitSalesDate:
                                 limitMonth = dateObj.strftime("%b %Y").upper()
-
-                        idx = meses.index(limitMonth)
-                        _meses = meses[:idx + 1]
-                        _forecast = forecast[:idx + 1]
-                        forecastReplica = pd.Series(data=_forecast, index=_meses)
-                        wo = eat(forecastReplica, stockAmount)
-                        dictMateriais[material]['Batch'][batch[0]]["Write off"] = wo
-                        print(wo,"oi")
+                        
+                        if limitMonth != None:
+                            idx = meses.index(limitMonth)
+                            if forecastReplica == None:
+                                _meses = meses[:idx + 1]
+                                _forecast = forecast[:idx + 1]
+                                forecastReplica = pd.Series(data=_forecast, index=_meses)
+                            print(forecastReplica)
+                            print(stockAmount)
+                            wo = eat(forecastReplica, stockAmount)
+                            dictMateriais[material]['Batch'][batch[0]]["Write off"] = wo
+                            print(batch,wo)
 
                 except:
                     traceback.print_exc()
